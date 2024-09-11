bd164afb7d3b6147b4a23c2d255b6b061a990aa9
unknown <zhjy0207@163.com>
2024-09-11 22:20:01 +0800

    port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request
 import sqlite3
 import datetime
+import os
 
 app=Flask(__name__)
 # 数据库初始化函数
@@ -72,4 +73,5 @@ def delDB():
 
 if __name__=='__main__':
     init_db()
-    app.run()
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
